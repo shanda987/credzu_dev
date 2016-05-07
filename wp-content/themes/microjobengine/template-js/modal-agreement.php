2a04edfed277647b04696cf88baa9e86ac489b88
jackbui <tambh@youngworld.vn>
2016-05-07 10:34:15 +0700

    dev modal agreement

---
+++ wp-content/themes/microjobengine/template-js/modal-agreement.php
@@ -0,0 +1,14 @@
+<div class="modal fade" id="agreement_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"><img
+                            src="<?php echo get_template_directory_uri() ?>/assets/img/icon-close.png" alt=""></span></button>
+                <h4 class="modal-title" id="myModalLabelForgot"></h4>
+            </div>
+            <div class="modal-body delivery-order">
+                body content here
+            </div>
+        </div>
+    </div>
+</div>
