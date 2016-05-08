0c9674b760f2321b5a4af6d144620b3a6db2d489
jackbui <tambh@youngworld.vn>
2016-05-08 15:25:33 +0700

    fix email

--- wp-content/themes/microjobengine/includes/modules/mJobMailing/class-mJobMailingAction.php
+++ wp-content/themes/microjobengine/includes/modules/mJobMailing/class-mJobMailingAction.php
@@ -21,7 +21,7 @@ class mJobMailingAction extends AE_Base
         $this->add_action('mjob_decline_order', 'mJobMailDeclineMjobOrder');
         $this->add_filter('ae_filter_receipt_mail_template', 'mJobMailFilterReceiptContent', 10, 3);
         $this->add_action('mjob_consumer_rights_email', 'mJobMailConsumerRights', 10, 2);
-        $this->add_action('mjob_consumer_right', 'mJobMailAgreement', 10, 2);
+        $this->add_action('mjob_agreement_email', 'mJobMailAgreement', 10, 2);
     }
 
     public function mJobMailRejectPost($args) {
