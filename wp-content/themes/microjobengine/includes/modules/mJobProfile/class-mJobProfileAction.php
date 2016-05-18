2ae6156c35af8604d2d8061d0fb44f5a60ae4605
jackbui <tambh@youngworld.vn>
2016-05-18 07:13:00 +0700

    fix bug

--- wp-content/themes/microjobengine/includes/modules/mJobProfile/class-mJobProfileAction.php
+++ wp-content/themes/microjobengine/includes/modules/mJobProfile/class-mJobProfileAction.php
@@ -112,7 +112,7 @@ class mJobProfileAction extends mJobPostAction
                 'msg' => __('Your account is pending. You have to activate your account to continue this step.', ET_DOMAIN)
             ));
         }
-        if( isset($request['routing_number']) || isset($request['account_number']) ) {
+        if( ( isset($request['routing_number']) && !empty($request['routing_number']) ) || (isset($request['account_number']) && !empty($request['account_number']))  ){
             $response = $this->getBankName($request['routing_number']);
             if( !$response['success'] ){
                 wp_send_json($response);
