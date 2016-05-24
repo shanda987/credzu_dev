0863750538e51dfe8b103cde5565b9e43839e0ee
jackbui <tambh@youngworld.vn>
2016-05-24 07:18:38 +0700

    dev credzu payment

--- wp-content/themes/microjobengine/includes/modules/credzuPayment/class-company-to-credzu.php
+++ wp-content/themes/microjobengine/includes/modules/credzuPayment/class-company-to-credzu.php
@@ -19,5 +19,49 @@ class companyToCredzu extends AE_Base{
     public  function __construct(){
 
     }
-
+    /**
+      * Get payment format
+      *
+      * @param void
+      * @return void
+      * @since 1.4
+      * @package MicrojobEngine
+      * @category CREDZU
+      * @author JACK BUI
+      */
+    public function getPaymentFormat(){
+        $args = array(
+            'post_type' => 'payment_format',
+            'meta_query' => array(
+                array(
+                    'key' => 'company_to_credzu',
+                    'value' => array('yes'),
+                )
+            ),
+            'post_status'=> 'publish',
+            'posts_per_page'=>1
+        );
+        $posts = get_posts( $args );
+        if( !isset($posts['0']) ){
+            return $posts['0'];
+        }
+        else{
+            return false;
+        }
+    }
+}
+if( !function_exists('companyToCredzu') ){
+    /**
+      * get instance of companyToCredzu class
+      *
+      * @param void
+      * @return object $instance
+      * @since 1.4
+      * @package MicrojobEngine
+      * @category CREDZU
+      * @author JACK BUI
+      */
+    function companyToCredzu(){
+        return companyToCredzu::getInstance();
+    }
 }
