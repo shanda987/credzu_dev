b0b91cd9fa832432f2055df7aa44e9629db17ddc
jackbui <tambh@youngworld.vn>
2016-05-29 00:07:49 +0700

    fix js bug for dashboard

--- wp-content/themes/microjobengine/assets/js/dashboard.js
+++ wp-content/themes/microjobengine/assets/js/dashboard.js
@@ -74,7 +74,8 @@
             legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
 
         };
-
-        var myNewChart = new Chart(ctx).Line(data, options);
+        if( $("#dashboard-chart").length > 0) {
+            var myNewChart = new Chart(ctx).Line(data, options);
+        }
     });
 })(window.AE.Views, window.AE.Models, window.AE.Collections, jQuery);
