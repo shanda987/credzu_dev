6cf541b4f3b4421408e601d619719ac66a25c0a9
jackbui <tambh@youngworld.vn>
2016-06-07 21:26:54 +0700

    fix google login

--- wp-content/themes/microjobengine/includes/aecore/social/template.php
+++ wp-content/themes/microjobengine/includes/aecore/social/template.php
@@ -74,12 +74,12 @@ function ae_page_social_connect(){
 	            <input type="submit" value="<?php _e('Submit', ET_DOMAIN);?>">
 	        </div>
 	    </form>
-		<div class="form_step2_auth">
+		<div class="form_step2_auth" method="post" action="">
 			<form id="form_auth_step2">
-			<input type="hidden" name="et_nonce" value="<?php echo wp_create_nonce( 'authentication' ) ?>">
-			<input type="hidden" name="user_login" value="<?php echo 'user_name_'.time() ?>" placeholder="<?php _e('Username', ET_DOMAIN) ?>">
-			<input type="hidden" name="first_name" value="<?php echo isset($auth['first_name']) ? $auth['first_name'] : "" ?>" >
-			<input type="hidden" name="last_name" value="<?php echo isset($auth['last_name']) ? $auth['last_name'] : "" ?>" >
+				<input type="hidden" name="et_nonce" value="<?php echo wp_create_nonce( 'authentication' ) ?>">
+				<input type="hidden" name="user_login" value="<?php echo 'user_name_'.time() ?>" placeholder="<?php _e('Username', ET_DOMAIN) ?>">
+				<input type="hidden" name="first_name" value="<?php echo isset($auth['first_name']) ? $auth['first_name'] : "" ?>" >
+				<input type="hidden" name="last_name" value="<?php echo isset($auth['last_name']) ? $auth['last_name'] : "" ?>" >
 			</form>
 		</div>
 	</div>
