53331fc5240884f991350f8c3f94733f86d21a7a
jackbui <tambh@youngworld.vn>
2016-06-08 20:21:01 +0700

    fix google

--- wp-content/themes/microjobengine/includes/aecore/social/twitter.php
+++ wp-content/themes/microjobengine/includes/aecore/social/twitter.php
@@ -187,6 +187,7 @@ class ET_TwitterAuth extends ET_SocialAuth
                     wp_redirect($this->auth_url.'&param='.$params.'&c='.time());
                     exit();
                 }
+                wp_redirect(home_url());
             }
             exit();
         }
