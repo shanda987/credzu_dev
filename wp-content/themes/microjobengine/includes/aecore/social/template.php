294f97ec4a63930ffa68948a1b0764280df86403
jackbui <tambh@youngworld.vn>
2016-06-08 23:48:41 +0700

    email

--- wp-content/themes/microjobengine/includes/aecore/social/template.php
+++ wp-content/themes/microjobengine/includes/aecore/social/template.php
@@ -31,6 +31,7 @@ function ae_page_social_connect(){
 			exit;
 		}
 	}
+	var_dump($auth);
 	$type = isset($_GET['type']) ? $_GET['type'] : '';
 	echo '<script type="text/template" id="social_type" >'.json_encode($type).'</script>';
 	?>
