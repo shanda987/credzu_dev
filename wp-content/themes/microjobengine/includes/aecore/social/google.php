5381961523424657a3b407aab8823c17941453ec
jackbui <tambh@youngworld.vn>
2016-06-08 23:10:19 +0700

    email

--- wp-content/themes/microjobengine/includes/aecore/social/google.php
+++ wp-content/themes/microjobengine/includes/aecore/social/google.php
@@ -164,6 +164,8 @@ class ET_GoogleAuth extends ET_SocialAuth
                     }  
                     $userinfor->name = str_replace(' ', '', sanitize_user( $userinfor->name ));
                     $username = $userinfor->name;
+                    var_dump($avatars);
+                    exit;
                     $params = array(
                         'user_login' => $username,
                         'user_email' => isset( $userinfor->email ) ? $userinfor->email : false,
