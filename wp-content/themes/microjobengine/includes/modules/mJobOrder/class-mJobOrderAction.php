a5192050b5deffa444cf4e48f6d9c606a1e81a09
jackbui <tambh@youngworld.vn>
2016-06-12 23:37:31 +0700

    dev reorder

--- wp-content/themes/microjobengine/includes/modules/mJobOrder/class-mJobOrderAction.php
+++ wp-content/themes/microjobengine/includes/modules/mJobOrder/class-mJobOrderAction.php
@@ -728,7 +728,7 @@ class mJobOrderAction extends mJobPostAction{
         $order_object = $ae_post_factory->get('mjob_order');
         $request = $_REQUEST;
         if( isset($request['order_id']) && !empty($request['order_id'])){
-            //$result = $this->updateOrderStatus($request['order_id'], 'verification');
+            $result = $this->updateOrderStatus($request['order_id'], 'processing');
             if( $result && !is_wp_error($result)){
                 $order = get_post($request['order_id']);
                 $order = $order_object->convert($order);
