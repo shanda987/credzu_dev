80da2f062623c76433078565e8c709049a32af71
jackbui <tambh@youngworld.vn>
2016-06-15 23:37:09 +0700

    fix module delivery

--- wp-content/themes/microjobengine/template-js/modal-delivery-order.php
+++ wp-content/themes/microjobengine/template-js/modal-delivery-order.php
@@ -7,39 +7,53 @@
                 <h4 class="modal-title" id="myModalLabelForgot"><?php _e('Provide Results', ET_DOMAIN) ?></h4>
             </div>
             <div class="modal-body delivery-order">
-                <div class="form-delivery-order">                         <div id="gallery_container" class="image-upload attachment-file attachment-file gallery_container inner-form" style="margin-bottom: 30px;">
+                <div class="form-delivery-order                         <div id="gallery_container" class="image-upload attachment-file attachment-file gallery_container inner-form" style="margin-bottom: 30px;">
+                
+                <p class="notice-message">
+                    <span class="note-action"><?php _e('Confirm your job is done.', ET_DOMAIN); ?></span><br/>
+                    <?php _e('By confirming that you have completed the work ou agreed to perform for your client. a payment from your client will be generated and they will informed. Please communicate with your client so they understand the difference between performing work and obtaining results.', ET_DOMAIN ) ?>
+                </p>
+                <form class="form-authentication">
+                    <div class="form-group">
+                        <textarea name="post_content" placeholder="Inactive text field"></textarea>
+                    </div>
+                    <!--                        <div class="attachment-file gallery_container" id="gallery_container">-->
+                    <!--                            <div class="attachment-image">-->
+                    <!--                                <ul class="gallery-image carousel-list carousel_deliver-image-list" id="image-list">-->
+                    <!--                                </ul>-->
+                    <!--                                <div>-->
+                    <!--                                    <div class="plupload_buttons" id="carousel_deliver_container">-->
+                    <!--                                        <span class="img-gallery" id="carousel_deliver_browse_button">-->
+                    <!--                                            <a href="#" class="add-img">--><?php //_e("Attach file", ET_DOMAIN); ?><!-- <i class="fa fa-plus"></i></a>-->
+                    <!--                                        </span>-->
+                    <!--                                    </div>-->
+                    <!--                                </div>-->
+                    <!--                                <span class="et_ajaxnonce" id="--><?php //echo wp_create_nonce( 'ad_carousels_et_uploader' ); ?><!--"></span>-->
+                    <!--                            </div>-->
+                    <!--                        </div>-->
+                    <div id="gallery_container" class="image-upload attachment-file attachment-file gallery_container" style="margin-bottom: 30px;">
+                        <div  class="browse_button carousel_container">
+                            <ul class="gallery-image carousel-list carousel_deliver-image-list requirement-image-list" id="image-list">
 
-                    <p class="notice-message">
-                        <span class="note-action"><?php _e('Confirm your job is done.', ET_DOMAIN); ?></span><br/>
-                        <?php _e('By confirming that you have completed the work ou agreed to perform for your client. a payment from your client will be generated and they will informed. Please communicate with your client so they understand the difference between performing work and obtaining results.', ET_DOMAIN ) ?>
-                    </p>
-                    <form class="form-authentication">
-                        <div class="form-group">
-                            <textarea name="post_content" placeholder="Inactive text field"></textarea>
-                        </div>
-                        <div id="gallery_container" class="image-upload attachment-file attachment-file gallery_container" style="margin-bottom: 30px;">
-                            <div  class="browse_button carousel_container">
-                                <ul class="gallery-image carousel-list carousel_deliver-image-list requirement-image-list" id="image-list">
-
-                                </ul>
-                                <div class="plupload_buttons" id="carousel_deliver_container">
-                                    <div class="drag-image" id="carousel_deliver_browse_button">
-                                        <i class="fa fa-cloud-upload"></i>
-                                        <p class="drag-image-title"><?php _e('Drag your <span class="requirement-modal-title-here"></span>') ?></p>
-                                        <span class="drag-image-text"><?php _e('or', ET_DOMAIN); ?></span>
-                                        <a class="drag-image-select-button"><?php _e('upload from local storage', ET_DOMAIN); ?></a>
-                                    </div>
+                            </ul>
+                            <div class="plupload_buttons" id="carousel_deliver_container">
+                                <div class="drag-image" id="carousel_deliver_browse_button">
+                                    <i class="fa fa-cloud-upload"></i>
+                                    <p class="drag-image-title"><?php _e('Drag your <span class="requirement-modal-title-here"></span>') ?></p>
+                                    <span class="drag-image-text"><?php _e('or', ET_DOMAIN); ?></span>
+                                    <a class="drag-image-select-button"><?php _e('upload from local storage', ET_DOMAIN); ?></a>
                                 </div>
                             </div>
-                            <span class="et_ajaxnonce" id="<?php echo wp_create_nonce( 'ad_carousels_et_uploader' ); ?>"></span>
                         </div>
-                        <div class="form-group">
-                            <input type="hidden" class="input-item post-service_nonce" name="_wpnonce" value="<?php echo de_create_nonce('ae-mjob_post-sync');?>" />
-                            <button class="btn-submit submit" disabled><?php _e('Send', ET_DOMAIN); ?></button>
-                        </div>
-                    </form>
-                </div>
+                        <span class="et_ajaxnonce" id="<?php echo wp_create_nonce( 'ad_carousels_et_uploader' ); ?>"></span>
+                    </div>
+                    <div class="form-group">
+                        <input type="hidden" class="input-item post-service_nonce" name="_wpnonce" value="<?php echo de_create_nonce('ae-mjob_post-sync');?>" />
+                        <button class="btn-submit submit" disabled><?php _e('Send', ET_DOMAIN); ?></button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
+</div>
 </div>
