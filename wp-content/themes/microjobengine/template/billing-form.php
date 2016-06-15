a65ec0bb95a1d771a9f34657e1d40f0085bd65a9
jackbui <tambh@youngworld.vn>
2016-06-15 20:14:33 +0700

    fix giact api

--- wp-content/themes/microjobengine/template/billing-form.php
+++ wp-content/themes/microjobengine/template/billing-form.php
@@ -13,13 +13,13 @@ echo mJobProfileAction()->getProfileJson($profile);
         <div class="form-group clearfix">
             <div class="input-group">
                 <div class="input-group-addon no-addon"><?php _e('Routing number:', ET_DOMAIN); ?></div>
-                <input type="password" name="routing_number" id="routing_number" placeholder="<?php _e('Routing number', ET_DOMAIN); ?>" value="<?php echo $profile->routing_number; ?>">
+                <input type="text" name="routing_number" id="routing_number" placeholder="<?php _e('***', ET_DOMAIN); ?>" value="<?php echo $profile->routing_number; ?>">
             </div>
         </div>
         <div class="form-group clearfix">
             <div class="input-group">
                 <div class="input-group-addon no-addon"><?php _e('Account Number:', ET_DOMAIN); ?></div>
-                <input type="password" name="account_number" id="account_number" placeholder="<?php _e('Account Number', ET_DOMAIN); ?>" value="<?php echo $profile->account_number; ?>">
+                <input type="text" name="account_number" id="account_number" placeholder="<?php _e('***', ET_DOMAIN); ?>" value="<?php echo $profile->account_number; ?>">
             </div>
         </div>
         <div class="form-group clearfix">
