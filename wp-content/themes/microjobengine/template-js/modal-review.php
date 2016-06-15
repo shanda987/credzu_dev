c59fc05df91eed30bfac34d70d59958e262b23dd
jackbui <tambh@youngworld.vn>
2016-06-15 22:58:46 +0700

    dev order

--- wp-content/themes/microjobengine/template-js/modal-review.php
+++ wp-content/themes/microjobengine/template-js/modal-review.php
@@ -14,26 +14,7 @@ $mjob_post = get_post($post->post_parent);
 				<h4 class="modal-title"><?php _e("Write review", ET_DOMAIN) ?></h4>
 			</div>
 			<div class="modal-body">
-			<form role="form" id="review_form" class="review-form">
-			 <?php
-			 	$employer_name = get_the_author_meta( 'display_name', $mjob_post->post_author );
-			  ?>
-                <div class="form-group">
-                    <span class="post_content"><?php printf(__('Rating',ET_DOMAIN),$mjob_post->post_title); ?> </span>
-                    <div class="rating-it" style="cursor: pointer;"></div>
-				</div>
-				<div class="form-group">
-					<textarea name="comment_content" cols="8" rows="10" placeholder="<?php _e('Your review here', ET_DOMAIN); ?>"></textarea>
-				</div>
-                <div class="form-group">
-                    <button type="submit" class="btn-submit btn-ok">
-						<?php _e('Send', ET_DOMAIN) ?>
-                    </button>
-					<button type="button" class="btn-skip btn-discard">
-						<?php _e('Skip', ET_DOMAIN) ?>
-					</button>
-				</div>
-				</form>
+			
 			</div>
 		</div><!-- /.modal-content -->
 	</div><!-- /.modal-dialog login -->
