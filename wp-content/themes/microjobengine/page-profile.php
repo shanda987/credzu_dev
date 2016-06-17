cd00906e6b4496b318fdb50ea9021e39d0d9e8fa
jackbui <tambh@youngworld.vn>
2016-06-17 20:01:12 +0700

    fix initial message

--- wp-content/themes/microjobengine/page-profile.php
+++ wp-content/themes/microjobengine/page-profile.php
@@ -120,57 +120,69 @@ echo mJobProfileAction()->display_company_status($user_role, $profile->company_s
                             <div class="input-group">
                                 <div class="input-group-addon no-addon"><?php _e('State:', ET_DOMAIN); ?></div>
                                 <select name="state" id="state" class="single-chosen">
-                                    <option value="Alabama">Alabama</option>
-                                    <option value="Alaska">Alaska</option>
-                                    <option value="Arizona">Arizona</option>
-                                    <option value="Arkansas">Arkansas</option>
-                                    <option value="CA">California</option>
-                                    <option value="California">Colorado</option>
-                                    <option value="Connecticut">Connecticut</option>
-                                    <option value="Delaware">Delaware</option>
-                                    <option value="District of Columbia">District of Columbia</option>
-                                    <option value="Florida">Florida</option>
-                                    <option value="Georgia">Georgia</option>
-                                    <option value="Hawaii">Hawaii</option>
-                                    <option value="Idaho">Idaho</option>
-                                    <option value="Illinois">Illinois</option>
-                                    <option value="Indiana">Indiana</option>
-                                    <option value="Iowa">Iowa</option>
-                                    <option value="Kansas">Kansas</option>
-                                    <option value="Kentucky">Kentucky</option>
-                                    <option value="Louisiana">Louisiana</option>
-                                    <option value="Maine">Maine</option>
-                                    <option value="Maryland">Maryland</option>
-                                    <option value="Massachusetts">Massachusetts</option>
-                                    <option value="MI">Massachusetts</option>
-                                    <option value="Minnesota">Minnesota</option>
-                                    <option value="Mississippi">Mississippi</option>
-                                    <option value="Missouri">Missouri</option>
-                                    <option value="Montana">Montana</option>
-                                    <option value="Nebraska">Nebraska</option>
-                                    <option value="Nevada">Nevada</option>
-                                    <option value="New Hampshire">New Hampshire</option>
-                                    <option value="New Jersey">New Jersey</option>
-                                    <option value="New Mexico">New Mexico</option>
-                                    <option value="New York">New York</option>
-                                    <option value="North Carolina">North Carolina</option>
-                                    <option value="North Dakota">North Dakota</option>
-                                    <option value="Ohio">Ohio</option>
-                                    <option value="Oklahoma">Oklahoma</option>
-                                    <option value="Oregon">Oregon</option>
-                                    <option value="Pennsylvania">Pennsylvania</option>
-                                    <option value="Rhode Island">Rhode Island</option>
-                                    <option value="South Carolina">South Carolina</option>
-                                    <option value="South DakotaD">South Dakota</option>
-                                    <option value="Tennessee">Tennessee</option>
-                                    <option value="Texas">Texas</option>
-                                    <option value="Utah">Utah</option>
-                                    <option value="Vermont">Vermont</option>
-                                    <option value="Virginia">Virginia</option>
-                                    <option value="Washington">Washington</option>
-                                    <option value="West Virginia">West Virginia</option>
-                                    <option value="Wisconsin">Wisconsin</option>
-                                    <option value="Wyoming">Wyoming</option>
+                                    <?php $sts = array(
+                                        'Alabama',
+                                        'Alaska',
+                                        'Arizona',
+                                        'Arkansas',
+                                        'California',
+                                        'Colorado',
+                                        'Connecticut',
+                                        'Delaware',
+                                        'District of Columbia',
+                                        'Florida',
+                                        'Georgia',
+                                        'Hawaii',
+                                        'Idaho',
+                                        'Illinois',
+                                        'Indiana',
+                                        'Iowa',
+                                        'Kansas',
+                                        'Kentucky',
+                                        'Louisiana',
+                                        'Maine',
+                                        'Maryland',
+                                        'Massachusetts',
+                                        'Michigan',
+                                        'Minnesota',
+                                        'Mississippi',
+                                        'Missouri',
+                                        'Montana',
+                                        'Nebraska',
+                                        'NevadaNew',
+                                        'HampshireNew',
+                                        'JerseyNew',
+                                        'MexicoNew',
+                                        'YorkNorth',
+                                        'Carolina',
+                                        'North',
+                                        'Dakota',
+                                        'Ohio',
+                                        'Oklahoma',
+                                        'Oregon',
+                                        'Pennsylvania',
+                                        'Rhode Island',
+                                        'South Carolina',
+                                        'South Dakota',
+                                        'Tennessee',
+                                        'Texas',
+                                        'Utah',
+                                        'Vermont',
+                                        'Virginia',
+                                        'Washington',
+                                        'West Virginia',
+                                        'Wisconsin',
+                                        'Wyoming'
+                                    );
+                                    foreach( $sts as $st ):
+                                        if( $profile->state == $st):
+                                            $selected = 'selected';
+                                        else:
+                                            $selected = '';
+                                        endif;
+                                    ?>
+                                    <option <?php echo $selected; ?> value="<?php echo $st; ?>"><?php echo $st; ?></option>
+                                    <?php endforeach; ?>
                                 </select>
 <!--                                <input type="text" name="state" id="state" placeholder="--><?php //_e('State', ET_DOMAIN); ?><!--" value="--><?php //echo $profile->state; ?><!--">-->
                             </div>
