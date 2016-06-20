732d22dca69849267df979b863f2857075ba95d5
jackbui <tambh@youngworld.vn>
2016-06-20 19:25:26 +0700

    fix category page

--- wp-content/themes/microjobengine/template/cat-block.php
+++ wp-content/themes/microjobengine/template/cat-block.php
@@ -7,7 +7,7 @@ $term = $obj->convert($term);
 ?>
 <div class="block-intro">
     <div class="container">
-        <p class="mjob_cat_content_css"><?php echo $term->mjob_category_page_content?></p>
+        <div class="mjob_cat_content_css"><?php echo $term->mjob_category_page_content?></div>
         <p class="block-title float-center"><?php echo $term->cat_bottom_title; ?></p>
         <ul>
             <li class="col-lg-4 col-md-4 col-sm-12 col-xs-12 clearfix wow fadeInUp">
