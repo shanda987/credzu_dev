6954ddc3c8a2bc7f1b5016e3ab1607627a42e404
jackbui <tambh@youngworld.vn>
2016-06-24 23:59:40 +0700

    fix payment

--- wp-content/themes/microjobengine/includes/modules/mJobOrder/class-mJobOrderAction.php
+++ wp-content/themes/microjobengine/includes/modules/mJobOrder/class-mJobOrderAction.php
@@ -245,7 +245,8 @@ class mJobOrderAction extends mJobPostAction{
         $author = get_userdata($mjob->post_author);
         $result->mjob_author = $mjob->post_author;
         $result->mjob_category = '';
-        var_dump($mjob->tax_input['mjob_category']);
+        echo '<pre>';
+        var_dump($mjob);
         if( isset($mjob->tax_input['mjob_category']['0']->name)){
             $result->mjob_category = $mjob->tax_input['mjob_category']['0']->name;
         }
