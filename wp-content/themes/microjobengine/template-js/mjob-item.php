4a646b06a25d49996d2e102422b7a57bc7efea24
jackbui <tambh@youngworld.vn>
2016-06-29 07:14:36 +0700

    fix status

--- wp-content/themes/microjobengine/template-js/mjob-item.php
+++ wp-content/themes/microjobengine/template-js/mjob-item.php
@@ -8,7 +8,7 @@
             <span class="total-review">({{= mjob_total_reviews }})</span>
         </div>
         <?php if(!is_search()) : ?>
-        <# if( is_featured != 1 && !is_search && !is_tax_mjob_category ){ #>
+        <# if( is_featured != 1 && !ae_globals.is_search && !ae_globals.is_tax_mjob_category ){ #>
         <div class="bookmark">
             <p class="marks {{= status_class }}">{{= status_text }}</p>
         </div>
