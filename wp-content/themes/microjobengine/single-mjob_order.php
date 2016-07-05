59c4d0b2c25016cc2a6b20e4eea3295ab79ba31d
jackbui <tambh@youngworld.vn>
2016-07-05 21:57:28 +0700

    dev pdf

--- wp-content/themes/microjobengine/single-mjob_order.php
+++ wp-content/themes/microjobengine/single-mjob_order.php
@@ -538,7 +538,7 @@ echo '<script type="text/template" id="order_single_data" >'.json_encode($curren
                                                         <?php if( isset($current->agreement_files) && !empty($current->agreement_files)):
                                                             foreach($current->agreement_files as $item): ?>
                                                                 <li class="col-lg-6 col-md-6 col-xs-12 item-requirement">
-                                                                    <a data-agreement="1" data-href="<?php echo $current->ID; ?>" href="#" data-name="<?php echo $item['name']?>" class="show-requirement-doc">
+                                                                    <a data-agreement="1" data-id="<?php echo $current->ID; ?>" href="#" data-name="<?php echo $item['name']?>" class="show-requirement-doc">
                                                                         <div class="doc-icon"> <i class="fa fa-file-pdf-o" aria-hidden="true"></i></div>
                                                                         <div class="doc-name"><?php echo $item['name'] ?></div>
                                                                         <div class="doc-time"><?php echo date('d/m/Y', strtotime($current->post_date))?></div>
