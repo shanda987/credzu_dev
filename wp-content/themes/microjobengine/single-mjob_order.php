40cf67f4e77ad15ea453886a8f3a53e4c9e75a36
jackbui <tambh@youngworld.vn>
2016-07-05 23:16:48 +0700

    dev pdf

--- wp-content/themes/microjobengine/single-mjob_order.php
+++ wp-content/themes/microjobengine/single-mjob_order.php
@@ -374,7 +374,7 @@ echo '<script type="text/template" id="order_single_data" >'.json_encode($curren
                                                                 endif;
                                                                 ?>
                                                                 <li class="col-lg-6 col-md-6 col-xs-12 item-requirement">
-                                                                    <a  href="#" data-name="<?php echo $term->name; ?>" data-id="<?php echo $current->ID; ?>" class="show-requirement-doc">
+                                                                    <a  href="#" data-name="<?php echo $term->name; ?>" data-id="<?php echo $f->ID; ?>" class="show-requirement-doc">
                                                                         <div class="doc-icon"> <i class="fa fa-file-pdf-o" aria-hidden="true"></i></div>
                                                                         <div class="doc-name"><?php echo $term->requirement_short_name.$tx?></div>
                                                                         <div class="doc-time"><?php echo date('d/m/Y', strtotime($f->post_date))?></div>
