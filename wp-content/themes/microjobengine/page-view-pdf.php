b8510d4f495735f09b9233b5dee14e58606bed6a
jackbui <tambh@youngworld.vn>
2016-07-05 23:42:25 +0700

    dev pdf

--- wp-content/themes/microjobengine/page-view-pdf.php
+++ wp-content/themes/microjobengine/page-view-pdf.php
@@ -25,9 +25,9 @@ else if( isset($_GET['cid'])) {
 else{
     $id = $_GET['pid'];
     $file = get_post_meta($id, 'pdf_path', false);
-    var_dump($file);
-    exit;
     $filename = basename($file);
+    var_dump($filename);
+    exit;
 }
 if( !empty($file) ) {
     header('Content-Type: application/pdf');
