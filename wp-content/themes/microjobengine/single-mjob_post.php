aca81c834a7fab55f222d2763fbd4067975399fa
jackbui <tambh@youngworld.vn>
2016-07-06 23:59:17 +0700

    Fix

--- wp-content/themes/microjobengine/single-mjob_post.php
+++ wp-content/themes/microjobengine/single-mjob_post.php
@@ -121,6 +121,70 @@ if($profile_id) {
                                     </a>
                                 </div>
                             </div>
+                            <div class="review-job">
+                                <p class="title">
+                                    <?php printf(__('Review <span class="total-review">(%s total)</span>', ET_DOMAIN), $count_review); ?>
+                                </p>
+                                <ul>
+                                    <?php
+                                    $reviews_per_page = 5;
+                                    $total_args =  array(
+                                        'type' => 'mjob_review',
+                                        'post_id' => $current->ID ,
+                                        'paginate' => 'load',
+                                        'order' => 'DESC',
+                                        'orderby' => 'date',
+                                    );
+
+                                    $query_args = wp_parse_args(array(
+                                        'number' => $reviews_per_page,
+                                        'page' => 1
+                                    ), $total_args);
+
+                                    // Get reviews
+                                    $review_obj = MjobReview::get_instance();
+                                    $reviews = $review_obj->fetch($query_args);
+                                    $reviews = $reviews['data'];
+                                    $review_data = array();
+
+                                    // Get total reviews
+                                    $total_reviews = count(get_comments($total_args));
+                                    // Get review pages
+                                    $review_pages  =   ceil($total_reviews/$query_args['number']);
+                                    $query_args['total'] = $review_pages;
+
+                                    if(!empty($reviews)):
+                                        foreach($reviews as $key => $value) {
+                                            $review_data[] = $value;
+                                            ?>
+                                            <li class="review-item clearfix">
+                                                <div class="image-avatar">
+                                                    <?php echo $value->avatar_user; ?>
+                                                </div>
+                                                <div class="profile-viewer">
+                                                    <a href="<?php echo $value->author_data->author_url; ?>" class="name-author">
+                                                        <?php echo $value->author_data->initial_display_name; ?>
+                                                    </a>
+                                                    <p class="review-time"><?php echo $value->date_ago; ?></p>
+                                                    <div class="rate-it star" data-score="<?php echo $value->et_rate; ?>"></div>
+                                                    <div class="commnet-content"><?php echo $value->comment_content;  ?></div>
+                                                </div>
+                                            </li>
+                                            <?php
+                                        }
+
+                                    endif; ?>
+                                </ul>
+
+                                <div class="paginations-wrapper" >
+                                    <?php
+                                    if($review_pages > 1) {
+                                        ae_comments_pagination($review_pages, $paged ,$query_args);
+                                    }
+                                    ?>
+                                </div>
+                                <?php echo '<script type="json/data" class="review-data" > ' . json_encode($review_data) . '</script>'; ?>
+                            </div>
                             <div class="information-items-detail">
                                 <div class="tabs-information">
                                     <span class="title"><?php _e('DESCRIPTION', ET_DOMAIN) ;?></span>
@@ -170,70 +234,6 @@ if($profile_id) {
                         </div>
                         <div class="mjob-edit-content">
                             <?php get_template_part('template-js/template', 'edit-mjob');?>
-                        </div>
-                        <div class="review-job">
-                            <p class="title">
-                                <?php printf(__('Review <span class="total-review">(%s total)</span>', ET_DOMAIN), $count_review); ?>
-                            </p>
-                            <ul>
-                                 <?php
-                                    $reviews_per_page = 5;
-                                    $total_args =  array(
-                                        'type' => 'mjob_review',
-                                        'post_id' => $current->ID ,
-                                        'paginate' => 'load',
-                                        'order' => 'DESC',
-                                        'orderby' => 'date',
-                                    );
-
-                                     $query_args = wp_parse_args(array(
-                                         'number' => $reviews_per_page,
-                                         'page' => 1
-                                     ), $total_args);
-
-                                    // Get reviews
-                                    $review_obj = MjobReview::get_instance();
-                                    $reviews = $review_obj->fetch($query_args);
-                                    $reviews = $reviews['data'];
-                                    $review_data = array();
-
-                                    // Get total reviews
-                                    $total_reviews = count(get_comments($total_args));
-                                    // Get review pages
-                                    $review_pages  =   ceil($total_reviews/$query_args['number']);
-                                    $query_args['total'] = $review_pages;
-
-                                    if(!empty($reviews)):
-                                        foreach($reviews as $key => $value) {
-                                            $review_data[] = $value;
-                                            ?>
-                                                <li class="review-item clearfix">
-                                                    <div class="image-avatar">
-                                                        <?php echo $value->avatar_user; ?>
-                                                    </div>
-                                                    <div class="profile-viewer">
-                                                        <a href="<?php echo $value->author_data->author_url; ?>" class="name-author">
-                                                            <?php echo $value->author_data->initial_display_name; ?>
-                                                        </a>
-                                                        <p class="review-time"><?php echo $value->date_ago; ?></p>
-                                                        <div class="rate-it star" data-score="<?php echo $value->et_rate; ?>"></div>
-                                                        <div class="commnet-content"><?php echo $value->comment_content;  ?></div>
-                                                    </div>
-                                                </li>
-                                            <?php
-                                        }
-
-                                        endif; ?>
-                            </ul>
-
-                            <div class="paginations-wrapper" >
-                                <?php
-                                if($review_pages > 1) {
-                                    ae_comments_pagination($review_pages, $paged ,$query_args);
-                                }
-                                ?>
-                            </div>
-                            <?php echo '<script type="json/data" class="review-data" > ' . json_encode($review_data) . '</script>'; ?>
                         </div>
                 </div>
                     <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 aside-detail-bar">
