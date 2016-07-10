796e227cec032409c749ee76cf6f750e20175e8a
jackbui <tambh@youngworld.vn>
2016-07-10 14:42:45 +0700

    fix payment check

--- wp-content/themes/microjobengine/sidebar-my-profile.php
+++ wp-content/themes/microjobengine/sidebar-my-profile.php
@@ -20,7 +20,7 @@ $display_name = isset($user_data->display_name) ? $user_data->display_name : '';
             <li class="hvr-wobble-horizontal"><a <?=(is_page('dashboard')) ? 'class="active"' : '' ?>" href="<?php echo et_get_page_link('dashboard'); ?>"><?php _e('Dashboard', ET_DOMAIN); ?></a></li>
             <?php if ($user_role == INDIVIDUAL ):?>
                 <li class="hvr-wobble-horizontal"><a <?=(is_page('profile')) ? 'class="active"' : '' ?>" href="<?php echo et_get_page_link('profile'); ?>"><?php _e('My Profile', ET_DOMAIN); ?></a></li>
-                <li class="hvr-wobble-horizontal"><a <?=(is_page('earn-money')) ? 'class="active"' : '' ?>" href="#"><?php _e('Earn money', ET_DOMAIN); ?></a></li>
+<!--                <li class="hvr-wobble-horizontal"><a --><?//=(is_page('earn-money')) ? 'class="active"' : '' ?><!--" href="#">--><?php //_e('Earn money', ET_DOMAIN); ?><!--</a></li>-->
                 <li class="line-distance"></li>
                 <li class="hvr-wobble-horizontal"><a <?=(is_page('billing-info')) ? 'class="active"' : '' ?>" href="<?php echo et_get_page_link('billing-info'); ?>"><?php _e('Billing information', ET_DOMAIN); ?></a></li>
             <?php elseif ($user_role == COMPANY ): ?>
