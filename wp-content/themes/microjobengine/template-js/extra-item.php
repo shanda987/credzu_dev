b0724c8ff0f9df6af310c26c82689a32ba35dcfc
jackbui <tambh@youngworld.vn>
2016-07-10 22:44:09 +0700

    dev create listing

--- wp-content/themes/microjobengine/template-js/extra-item.php
+++ wp-content/themes/microjobengine/template-js/extra-item.php
@@ -5,6 +5,7 @@
                 <label>
                     <input data-id="{{= ID}}" data-featured="0" type="checkbox" value="{{= et_budget}}" name="mjob_extra">
                     <span>{{= post_title}}</span>
+                    <div class="extra-content">{{= post_content }}</div>
                 </label>
             </div>
         </div>
