9e21d7a1b184f39cede6d554ae0ed847fff3ad7b
jackbui <tambh@youngworld.vn>
2016-07-17 16:30:37 +0700

    add date

--- wp-content/themes/microjobengine/template-js/modal-work-complete.php
+++ wp-content/themes/microjobengine/template-js/modal-work-complete.php
@@ -15,8 +15,8 @@
                                 <span class="note-body"><?php _e('By confirming that you have completed the work you agreed to perform for your client, a payment from your client will be generated and they will informed. Please communicate with your client so they understand the difference between performing work and obtaining results.', ET_DOMAIN ) ?></span>
                             </p>
                             <p>
-                                <div class="input-group col-md-3 col-xs-12"><label for="work_complete_date"> DATE </label> </div>
-                                <div class="col-md-9 col-xs-12"> <input type="text"  name="work_complete_date" id="work_complete_date" placeholder="<?php _e('DATE', ET_DOMAIN); ?>" value=""></div>
+                                <div class="input-group"><label for="work_complete_date"> DATE </label> </div>
+                                <div class=""> <input type="text"  name="work_complete_date" id="work_complete_date" placeholder="<?php _e('DATE', ET_DOMAIN); ?>" value=""></div>
                             </p>
                         </div>
                     </div>
