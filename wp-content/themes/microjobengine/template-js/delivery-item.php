fe11abc387af99440159a99117fb660662e64fce
jackbui <tambh@youngworld.vn>
2016-07-30 19:55:40 +0700

    Fix

---
+++ wp-content/themes/microjobengine/template-js/delivery-item.php
@@ -0,0 +1,5 @@
+<script type="text/template" id="addmore-item-loop">
+    <div class="form-group list-item-extra">
+        <textarea rows="3" name="addmore_item" class="addmore-item-text"></textarea>
+    </div>
+</script>
