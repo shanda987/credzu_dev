1605e734f0e85b7990740bf3fde6db4525db3c37
jackbui <tambh@youngworld.vn>
2016-08-01 07:15:17 +0700

    fix

--- wp-content/themes/microjobengine/includes/modules/mJobOrder/class-mJobOrderAction.php
+++ wp-content/themes/microjobengine/includes/modules/mJobOrder/class-mJobOrderAction.php
@@ -393,7 +393,7 @@ class mJobOrderAction extends mJobPostAction{
         if (empty($result->uploaded)) {
             add_post_meta($result->ID, 'uploaded', ' ');
         }
-        if( $result->can_review == 1 || $result->can_review ){
+        if( $result->can_review == 1 || $result->can_review == true ){
             $result->can_review = true;
         }
         else{
